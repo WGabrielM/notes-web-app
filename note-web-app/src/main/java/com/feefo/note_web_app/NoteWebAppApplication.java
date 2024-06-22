b7521b40d26d5dd1bbd55272ebd8b16e0dc12c7a
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-22 12:04:23 -0300

    git init

---
+++ note-web-app/src/main/java/com/feefo/note_web_app/NoteWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.feefo.note_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteWebAppApplication.class, args);
+	}
+
+}
