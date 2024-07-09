40159145742b7c02086fbc11449c4cb9d3762e7c
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-07-09 09:00:05 -0300

    create backend template with spring initializr

---
+++ notes-web-app-backend/src/test/java/com/assessment/notes_web_app_backend/NotesWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.notes_web_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
