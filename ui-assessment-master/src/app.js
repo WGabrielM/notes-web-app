b6d1799a13cdadb9122a36efdb8941cc3bb1b5bb
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-07-10 22:36:59 -0300

    add structure

--- ui-assessment-master/src/app.js
+++ ui-assessment-master/src/app.js
@@ -1,26 +1,50 @@
-import './app.css';
-import AccountOverview from './account-overview';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { Email, Header, HomeContainer, SupportContact } from "./styles";
+
+import "./app.css";
 
 function App() {
-  const accountOverviewStub = {
-    supportContact: {
-      name: 'John Smith',
-      email: 'john.smith@feefo.com'
-    },
-    salesOverview: {
-      uploads: 8,
-      successfulUploads: 3,
-      linesAttempted: 20,
-      linesSaved: 4,
-      lastUploadDate: 1605001226079,
-    }
-  }
-
-
   return (
-    <div className="App">
-      <AccountOverview data={accountOverviewStub}/>
-    </div>
+    <HomeContainer>
+      <Header>
+        <h1>Account Overview</h1>
+        <SupportContact>
+          <span>your feefo support contact</span>
+          <div>
+            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+            <Email>
+              <span>Support</span>
+              <p>support@feefo.com</p>
+            </Email>
+            <p>020 3362 4208</p>
+          </div>
+        </SupportContact>
+      </Header>
+      <div>
+        <div>
+          <strong>Sales</strong>
+
+          <p>You had 0 uploads and 0 lines added.</p>
+        </div>
+
+        <div>
+          <strong></strong>
+          <span>upload success</span>
+        </div>
+        <div>
+          <strong></strong>
+          <span>lines saved</span>
+        </div>
+      </div>
+
+      <article>
+        <form>
+          <textarea name="task" placeholder="Add a new note" required />
+          <button type="submit">Save</button>
+        </form>
+      </article>
+    </HomeContainer>
   );
 }
 
---
+++ ui-assessment-master/src/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    margin: 0 auto;
+
+    width: 1280px;
+`
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+export const SupportContact = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+`
+
+export const Email = styled.div`
+    margin-top: 15px;
+`
